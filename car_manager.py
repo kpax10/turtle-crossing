58fabde491dba5d7947337525c5577e2b394769b
Kyle <notxap55@yahoo.com>
2024-08-02 16:31:35 -0700

    turtle added, cars added

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,28 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.shapesize(1, 2)
+        self.setheading(180)
+        self.goto(290, random.randint(-250, 250))
+        self.move()
+
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
+
+# TODO 2: Create cars that are 20px high by 40px wide that are randomly generated along
+#  the y-axis and move to the left edge of the screen. No cars should be generated in
+#  the top and bottom 50px of the screen (think of it as a safe zone for our little
+#  turtle). Hint: generate a new car only every 6th time the game loop runs. If you
+#  get stuck, check the video walkthrough in Step 4.
--- main.py
+++ main.py
@@ -4,11 +4,36 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+SLEEP_TIME = 0.1
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+screen.onkey(key='w', fun=player.move)
+
+counter = 0
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(SLEEP_TIME)
     screen.update()
+    counter += 1
+    if counter == 6:
+        car = CarManager()
+
+    # TODO 1: Counter not working
+
+
+    # TODO 3: Detect when the turtle player collides with a car and stop the game if this happens
+
+    # TODO 4: Detect when the turtle player has reached the top edge of the screen (i.e.,
+    #  reached the FINISH_LINE_Y). When this happens, return the turtle to the starting position
+    #  and increase the speed of the cars. Hint: think about creating an attribute and using the
+    #  MOVE_INCREMENT to increase the car speed
+
+    # TODO 5: Create a scoreboard that keeps track of which level the user is on. Every time the
+    #  turtle player does a successful crossing, the level should increase. When the turtle hits
+    #  a car, GAME OVER should be displayed in the centre
